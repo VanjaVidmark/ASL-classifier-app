a021fe322d469741ba760ab8b6d09d8fadf13c0d
Vanja Vidmark <vanjavidmark@Administrators-MacBook-Pro.local>
2025-02-19 21:14:30 +0100

    requirements and UI fixes

--- asl-react-app/src/ClassifierView.js
+++ asl-react-app/src/ClassifierView.js
@@ -1,14 +1,11 @@
-import './classifier.css';
-
 export default function ClassifierView(props) {
     return (
         <section className="classifier-view">
-            <link rel="stylesheet" href="classifier.css"/>
             <h1 className="title">ASL Classifier</h1>
             <p className="explanation">Test the classifier by ...</p>
             <div className="camera-container">
-                <video ref={props.video} className="video" style={{ transform: 'scaleX(-1)' }}></video>
-                <p>{"Guessed letter: " + props.prediction}</p>
+                <video ref={props.video} className="video"></video>
+                <p className="guessed-text">{"Guessed letter: " + props.prediction}</p>
             </div>
         </section>
     );
